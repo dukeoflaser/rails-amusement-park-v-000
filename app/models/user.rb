72cb6aaddd411a5b2b8bcf6fbd42681f605259eb
Nathaniel Miller <dukeoflaser@gmail.com>
2016-06-18 17:19:08 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,4 @@ class User < ActiveRecord::Base
 
   has_many :rides
   has_many :attractions, through: :rides
-
-  # validates :name, :height, :happiness,
-  #           :tickets, :password,
-  #           presence: true
 end
